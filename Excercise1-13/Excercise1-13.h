d18336fe5fde95d8b995e2c64d9a0a40bb0330ec
esmeralda-gallardoRGB <72875576+esmeralda-gallardoRGB@users.noreply.github.com>
2020-11-13 19:42:32 -0300

    Solved horizontal histogram
    Vertical histogram pending

--- Excercise1-13/Excercise1-13.h
+++ Excercise1-13/Excercise1-13.h
@@ -6,5 +6,18 @@
 // Write a program to print a (horizontal) histogram of the lengths of words in its input. 
 int main () {
 
+	int c;
+
+	while ((c = getchar()) != EOF) {
+		if(c == ' ' || c == '\t' || c == '\n'){
+			 putchar('\n');
+		}
+    	else {
+      		putchar('*');
+    	}
+
+	}
 
 }
+
+#endif
