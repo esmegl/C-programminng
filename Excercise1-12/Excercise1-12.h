6334a3e93832490ffb0f40fd87b9301645d48a2c
esmeralda-gallardoRGB <72875576+esmeralda-gallardoRGB@users.noreply.github.com>
2020-11-13 18:01:31 -0300

    Add Excercise1-12
    Program that prints its input on one word per line.

---
+++ Excercise1-12/Excercise1-12.h
@@ -0,0 +1,30 @@
+#ifndef _Excercise_1_12_
+#define _Excercise_1_12_
+
+#define IN   1  /* inside a word */
+#define OUT  0  /* outside a word */
+
+#import <stdio.h>
+// Program that prints its input one word per line.
+
+int main () {
+
+	int c, nw, state;
+	
+	state = OUT;
+	while ((c = getchar()) != EOF){
+
+		if (c == ' ' || c == '\n' || c == '\t') {
+			state = OUT;
+			putchar('\n');
+		}
+		else {
+			putchar(c);
+		}
+
+	}
+
+}
+
+
+#endif
