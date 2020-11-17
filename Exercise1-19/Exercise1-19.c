98246911206526a797a5a25fb4e2a9bba3f970e1
esmeralda-gallardoRGB <72875576+esmeralda-gallardoRGB@users.noreply.github.com>
2020-11-17 18:53:10 -0300

    Add Exercise1-19
    Write a function reverse(s) that reverses the character string s. Use it to write a program that reverses itsinput a line at a time.

---
+++ Exercise1-19/Exercise1-19.c
@@ -0,0 +1,1 @@
+#include "Exercise1-19.h"
---
+++ Exercise1-19/Exercise1-19.h
@@ -0,0 +1,14 @@
+#ifndef _EXERCISE_1_19_
+#define _EXERCISE_1_19_
+
+#include <stdio.h>
+
+
+// Write a function reverse(s) that reverses the character string s. 
+//Use it to write a program that reverses itsinput a line at a time.
+int main () {
+
+
+}
+
+#endif
