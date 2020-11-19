2f6524fb82e23825d7d7134f996b2880d22dbc32
esmeralda-gallardoRGB <72875576+esmeralda-gallardoRGB@users.noreply.github.com>
2020-11-19 20:02:30 -0300

    Fix segmentation fault error
    Pending: fix reverse() function.

--- Exercise1-19/Exercise1-19.h
+++ Exercise1-19/Exercise1-19.h
@@ -6,39 +6,57 @@
 
 // Write a function reverse(s) that reverses the character string s. 
 //Use it to write a program that reverses itsinput a line at a time.
-void reverse(char s[]);
+void reverse(char line[]);
 
 int main() {
 
-	char line[MAXLINE]; // Original array.
+	char line[MAXLINE];
 	int c, i, r;
 
+	// Initialize the list.
+	for (i = 0; i <= MAXLINE; ++i)
+		line[i] = 0;
+
 	i = 0;
+
+	//Debuggin' in progress
+	// Until here the code executes correctly
 	printf("Input your array: \n");
 
 	while ((c = getchar()) != EOF ) {
 		
 		// Checks if the line hasn't finished.
-		while (c != '\n'){
+		while ((c != '\n') && (i < MAXLINE)){
+			printf("%d\n", i);
 			line[i] = c;
 			++i;
 		}
+
 		// If the line finished start again, i = 0.
 		if (c == '\n'){
-			line[i] = '\0';
 			i = 0;
 		}
-		// Print the results.
-		reverse(line);
 	}
+
+	// Print the results.
+	reverse(line);
 
 }
 
-void reverse(char s[]) {
-	int r;
-	for (r = MAXLINE; ; --r) {
-		putchar(s[r]);
+void reverse(char line[]) {
+	int r, i;
+	char rev[MAXLINE];
+
+	i = MAXLINE;
+
+	for (r = 0; r <= MAXLINE ; ++r) {
+		if ( i >= 0){
+			rev[r] = line[i];
+			--i;
+		}
+
 	}
+	printf("%d\n ", rev[r]);
 }
 
 
