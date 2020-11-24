419c6fd8b3bbf427d84354d9a6453e980bde53cf
esmeralda-gallardoRGB <72875576+esmeralda-gallardoRGB@users.noreply.github.com>
2020-11-24 19:21:01 -0300

    Solve Exercise1-19

--- Exercise1-19/Exercise1-19.h
+++ Exercise1-19/Exercise1-19.h
@@ -6,21 +6,25 @@
 
 // Write a function reverse(s) that reverses the character string s. 
 //Use it to write a program that reverses its input a line at a time.
-char reverse(char line[], char rev[]);
+
+char rev[MAXLINE + 1]; /* Stores the elements in reverse order */
+
+char reverse(char line[]);
 
 int main() {
 
+	extern char rev[];
+
 	char line[MAXLINE + 1];
 	char c;
-	char rev[MAXLINE + 1]; /* rev[] will store the array line[] in an inverse order. */
 
 	for (int i = 0; (i <= MAXLINE); ++i) /* Innitialize rev[] */
 		rev[i] = '\0';
 
 	printf("Input your array: \n");
 
-	for (int i = 0; (i <= MAXLINE); ++i) /* Initializes line[] */
-		line[i] = '\0';
+	for (int k = 0; (k <= MAXLINE); ++k) /* Initializes line[] */
+		line[k] = '\0';
 	
 	while ((c = getchar()) != EOF) { /* Main loop */
 		int j = 0;
@@ -34,7 +38,11 @@ int main() {
 
 		printf("Line is: \n");
 		printf("%s\n", line); /* Print line[] */
-		reverse(line, rev); 
+		reverse(line); 
+		printf("Reverse is: \n");
+		for (int h = 0; (h < MAXLINE); ++h)
+			putchar(rev[h]); /* Print rev[] */
+			printf("\n");
 		
 
 	}
@@ -42,8 +50,9 @@ int main() {
 
 } /* End of main(). */
 
-//Debuggin' in progress
-char reverse(char line[], char rev[]) {
+char reverse(char line[]) {
+
+	extern char rev[];
 
 	printf("Inside reverse()\n");
 	int j = (MAXLINE - 1);
@@ -53,8 +62,6 @@ char reverse(char line[], char rev[]) {
 		--j;
 
 	}
-	printf("Reverse is: \n");
-	printf("%s\n", rev); /* Print rev[] */
 	return 0;
 }
 
